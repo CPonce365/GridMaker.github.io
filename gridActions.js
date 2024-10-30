dbfd5efd72af15e35eaadef2d3c84272bd4dc0af
CPonce365 <145749902+CPonce365@users.noreply.github.com>
2024-10-30 13:37:06 -0400

    Added the proper grid functions for each button

--- gridActions.js
+++ gridActions.js
@@ -1,37 +1,124 @@
 let numRows = 0;
 let numColumns = 0;
-let colorselected;
+let colorSelected;
 
-function addRow() {
-    alert("Clicked on add Row");
+function addRow(){
+    const grid = document.getElementById("grid");
+    const row = grid.insertRow();
+
+    if(numRows == 0 && numColumns == 0){
+        const firstCell =  row.insertCell();
+        const cells = document.querySelectorAll("td");
+        cells[0].onclick = function(){
+            cells[0].style.backgroundColor = colorSelected;
+        }
+        numColumns++;
+        numRows++;
+    }
+    else{
+        for(let i = 0; i < numColumns; i++){
+            const rowCells = row.insertCell(); 
+        }
+        const cells = document.querySelectorAll("td");
+        for(let i = 0; i < cells.length; i++){
+            cells[i].onclick = function(){
+                cells[i].style.backgroundColor = colorSelected;
+            }
+        }
+        numRows++;
+    }
 }
 
 function addColumn() {
-    alert("clicked on add Collumn")
+    const grid = document.getElementById("grid");
+    const column = document.getElementsByTagName("tr");
+
+    if(numRows == 0 && numColumns == 0){
+        const firstRow = grid.insertRow();
+        const firstCell = firstRow.insertCell();
+        const cells = document.querySelectorAll("td");
+        cells[0].onclick = function(){
+            cells[0].style.backgroundColor = colorSelected;
+        }
+        numRows++;
+        numColumns++;
+    }
+    else{
+        for(let i = 0; i < numRows; i++){
+            const columCells = column[i];
+            columCells.insertCell();
+        }
+        const cells = document.querySelectorAll("td");
+        for(let i = 0; i < cells.length; i++){
+            cells[i].onclick = function(){
+                cells[i].style.backgroundColor = colorSelected;
+            }
+        }
+        numColumns++;
+    }
 }
 
 function removeRow() {
-    alert("Clicked on remove Row");
+    const grid = document.getElementById("grid");
+    if(numRows > 0){
+        grid.deleteRow(numRows - 1);
+        numRows--;
+    }
+    if(numRows < 0){
+        numColumns = 0;
+        numRows = 0;
+    }
+    if(numRows == 0){
+        alert("There are no more rows to remove!!!");
+    }
 }
 
 function removeColumn() {
-    alert("Clicked on remove Column");
+    const grid = document.getElementById("grid");
+    const columns = grid.getElementsByTagName("tr");
+
+    if(numColumns > 0){
+        for(let i = 0; i < columns.length; i++){
+            columns[i].deleteCell(numColumns - 1);
+        }
+        numColumns--;
+    }
+    if(numColumns < 0){
+        numColumns = 0;
+        numRows = 0;
+    }
+    if(numRows == 0){
+        alert("There are no more columns to remove!!!");
+    }
 }
 
 function selectColor(){
     colorSelected = document.getElementById("selectedColorId").value;
-    console.log(colorSelected);
+    document.getElementById("colorchosen").innerHTML = "You selected the color: " + colorSelected;
 }
 
 function uncoloredCells() {
-    alert("Clicked on uncolor all cells");
+    const color = document.getElementById("selectedColorId").value;
+    const cells = document.querySelectorAll("td");
+    for(let i = 0; i < cells.length; i ++){
+        if(cells[i].style.backgroundColor === "" || cells[i].style.backgroundColor === "white"){
+            cells[i].style.backgroundColor = color;
+        }
+    }
 }
 
 function colorAllCells() {
-    alert("Clicked on color all cells");
+    const color = document.getElementById("selectedColorId").value;
+    const cells = document.querySelectorAll("td");
+    for(let i = 0; i < cells.length; i ++){
+        cells[i].style.backgroundColor = color;
+    }
 }
 
 function clearCells() {
-    alert("Clicked on clear all cells of color");
+   const cells = document.querySelectorAll("td");
+   for(let i = 0; i < cells.length; i++){
+    cells[i].style.backgroundColor = "";
+   }
 }
 
